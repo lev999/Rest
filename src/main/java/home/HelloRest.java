c9ceae234e775686fab66009182cb2b4f2766bb7
Lev999 <lev.magazinnik@gmail.com>
2015-06-26 11:10:34 -0300

    try push

--- src/main/java/home/HelloRest.java
+++ src/main/java/home/HelloRest.java
@@ -9,7 +9,7 @@ import javax.ws.rs.core.MediaType;
  * Root resource (exposed at "myresource" path)
  */
 @Path("helloRest")
-public class HelloRest {
+public class HelloRest  {
 
     /**
      * Method handling HTTP GET requests. The returned object will be sent
