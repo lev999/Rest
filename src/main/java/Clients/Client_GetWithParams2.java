da392e1fb6902d8383aeb17078c0c05a9ec29aa7
Lev999 <lev.magazinnik@gmail.com>
2015-06-27 17:55:16 -0300

    add PathParam client +service

--- src/main/java/Clients/Client_GetWithParams2.java
+++ src/main/java/Clients/Client_GetWithParams2.java
@@ -6,20 +6,17 @@ import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.core.Response;
 
-public class Client_GetWithParams extends BaseClass{
+public class Client_GetWithParams2 extends BaseClass{
     public static void main(String[] args) {
 
         Client client = ClientBuilder.newClient();
 
         Response response =client
                 .target(PATH)
-                .path("getWithParams")
-                .queryParam("name","Vasia")
-                .queryParam("lastName","Pupkin")
+                .path("getWithParams/id/12")
                 .request()
                 .get();
 
-//                .post(javax.ws.rs.client.Entity.entity("My POSTing text", MediaType.TEXT_PLAIN_TYPE));
         System.out.println("STATUS:"+response.getStatus());
         System.out.println("RESPONSE:"+response.readEntity(String.class));
     }
--- src/main/java/home/GetWithParams.java
+++ src/main/java/home/GetWithParams.java
@@ -9,10 +9,20 @@ import javax.ws.rs.core.MediaType;
 @Path("getWithParams")
 public class GetWithParams {
 
+    @DefaultValue("nonName")@QueryParam("lastName")
+    String lastName;
     @GET
     @Produces("text/plain")
     @Consumes("text/plain")
     public String getWithParams(@DefaultValue("nonName")@QueryParam("name") String name) {
-        return "got name=" + name;
+        return "got name=" + name+" last name="+lastName;
+    }
+
+    @Path("/id/{id}")
+    @GET
+    public String getId(@PathParam("id")int id){
+
+        return "getId(): id="+id;
+
     }
 }
